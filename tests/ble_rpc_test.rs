501b9c7e6084896121c463a4bee2f0dd75b8e8bb
Tyler Potyondy <potyondyt@gmail.com>
2026-02-27 12:50:26 -0800

    tests: add ble rpc init test

---
+++ tests/ble_rpc_test.rs
@@ -0,0 +1,45 @@
+extern crate alloc;
+
+mod utils;
+use alloc::{boxed::Box, rc::Rc, vec::Vec};
+use core::cell::RefCell;
+use nrf_rpc::ble::Ble;
+
+#[test]
+fn test_rpc_init_packets() {
+    let mock_uart = utils::MockUart {
+        transmitted: Rc::new(RefCell::new(Vec::new())),
+    };
+
+    let mock_uart_clone = mock_uart.clone();
+    let result = utils::block_on(Box::pin(async { Ble::new(mock_uart_clone).await }));
+
+    assert!(result.is_ok(), "Ble::new() should succeed during init");
+
+    // Check that the packet is correct.
+    // Correct init packet should be: {04 00 ff 00 ff 00 62 74  5f 72 70 63}
+    let bt_rpc_init_packet = [
+        0x04, 0x00, 0xFF, 0x00, 0xFF, 0x00, b'b', b't', b'_', b'r', b'p', b'c',
+    ];
+    let rpc_utils_init_packet = [
+        0x04, 0x00, 0xFF, 0x01, 0xFF, 0x00, b'r', b'p', b'c', b'_', b'u', b't', b'i', b'l', b's',
+    ];
+
+    let expected_data = [
+        bt_rpc_init_packet.as_slice(),
+        rpc_utils_init_packet.as_slice(),
+    ]
+    .concat();
+
+    let transmitted = mock_uart.transmitted.borrow();
+    // Check that the transmitted packet matches the expected init packet.
+    assert_eq!(
+        transmitted.as_slice(),
+        expected_data.as_slice(),
+        "[Test Failure] - Transmitted packet should match expected init packet. 
+        Expected: {:02x?}, 
+        Got:      {:02x?}",
+        expected_data.as_slice(),
+        transmitted.as_slice()
+    );
+}
---
+++ tests/utils/mod.rs
@@ -0,0 +1,58 @@
+extern crate alloc;
+
+use alloc::rc::Rc;
+use alloc::vec::Vec;
+use core::cell::RefCell;
+use core::future::Future;
+use core::pin::Pin;
+use core::ptr;
+use core::task::{Context, Poll, RawWaker, RawWakerVTable};
+use nrf_rpc::{AsyncTransport, TransportError};
+
+/// Simple blocking executor for tests - only works for futures that are immediately ready
+pub fn block_on<F: Future + Unpin>(mut fut: F) -> F::Output {
+    unsafe fn noop_clone(_: *const ()) -> RawWaker {
+        noop_raw_waker()
+    }
+
+    unsafe fn noop(_: *const ()) {}
+
+    fn noop_raw_waker() -> RawWaker {
+        const VTABLE: RawWakerVTable = RawWakerVTable::new(noop_clone, noop, noop, noop);
+        RawWaker::new(ptr::null(), &VTABLE)
+    }
+
+    let waker = unsafe { core::task::Waker::from_raw(noop_raw_waker()) };
+    let mut ctx = Context::from_waker(&waker);
+
+    loop {
+        match Pin::new(&mut fut).poll(&mut ctx) {
+            Poll::Ready(val) => return val,
+            Poll::Pending => panic!("Future should be immediately ready in tests"),
+        }
+    }
+}
+
+/// Mock UART transport for testing - returns immediately
+#[derive(Clone)]
+pub struct MockUart {
+    pub transmitted: Rc<RefCell<Vec<u8>>>,
+}
+
+#[derive(Debug)]
+pub struct MockError;
+
+impl TransportError for MockError {}
+
+impl AsyncTransport for MockUart {
+    type Error = MockError;
+
+    async fn write(&mut self, data: &[u8]) -> Result<usize, Self::Error> {
+        self.transmitted.borrow_mut().extend_from_slice(data);
+        Ok(data.len())
+    }
+
+    async fn read(&mut self, _buffer: &mut [u8]) -> Result<usize, Self::Error> {
+        Ok(0)
+    }
+}
